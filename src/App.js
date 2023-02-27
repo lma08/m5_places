9a9b036d72b5535c3c00218ebe8427f26ac06ea5
lma08 <mlidzhieva@gmail.com>
2023-02-27 13:55:34 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+import { data } from './data';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [places, setPlaces] = useState(data);
+
+    const removeItem = (id) => {
+        let newPlaces = places.filter ((item) => item.id !== id);
+        setPlaces (newPlaces)
+    }
+
+    const [showText, setShowText]=useState(false)
+
+    const showTextClick = (text) => {
+        text.showMore = !text.showMore
+        setShowText(!showText)
+    }
+
+    return (
+        <div>
+            <div className='container'>
+                <h1>Top {places.length} must visit places in Toronto</h1>
+            </div>
+                {places.map ((element => {
+                    const {id, name, description, image1, showMore} = element;
+
+                    return(
+                        <div key={id}>
+                            <div className='container'>
+                                <h2>{id}. {name}</h2>
+                            </div>
+                            <div className='container'>
+                                <img src={image1} alt='place' width='450px'/>
+                            </div>
+                            <div className='container'>
+                                <h4>{showMore ? description: description.substring(0,220)+'...'} <button className='more' onClick={() => showTextClick(element)}>
+                                        {showMore ? 'Show less' : 'Show more'}
+                                    </button>
+                                </h4>
+                            </div>
+                            <div className='container'>
+                                <button className='btn' onClick={() => removeItem(id)}>Remove</button>
+                            </div>
+                        </div>
+                    )
+                }))}
+            <div className='container'>
+                <button className='btnAll' onClick={() => setPlaces([])}>Delete All</button>
+            </div>
+        </div>
+    )
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,30 @@
+export const data = [
+    {
+        id:1,
+        name:'The CN Tower',
+        description:`Toronto's famous landmark, the 553-meter CN Tower, is one of Ontario's must-see attractions and also the most impossible to miss. Towering above the downtown, this Canadian icon can be seen from almost everywhere in the city. 
+            You have the option of simply appreciating the building from the ground, or taking a trip up to one of the 
+            observation areas or restaurants for fabulous views over the city and Lake Ontario. The CN Tower, 
+            built between 1972 and 1976, was once the tallest freestanding structure in the world, but has long since 
+            been surpassed.`,
+        image1:'https://images.unsplash.com/photo-1632857997897-9418428d7368?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+        showMore:false
+    },
+    {
+        id:2,
+        name:`The Royal Ontario Museum (ROM)`,
+        description:`The Royal Ontario Museum, known as the ROM, is one of Canada's premier museums and one of the top tourist attractions in Ontario, with an international reputation for excellence.
+            It houses an outstanding collection, which covers natural history, art, and culture from a great variety of periods from all over the world. It is also well-known for featuring exhibitions from across the globe.
+            A controversial expansion in 2007 saw the addition of the Michael Lee-Chin Crystal, a modern wing featuring glass and sharp angles, added on to a very traditional older building. It's now one of Toronto's most recognizable buildings.`,
+        image1:'https://images.unsplash.com/photo-1579681093661-df46621ca0f7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80',
+        showMore:false
+    },
+    {
+        id:3,
+        name:'Art Gallery of Ontario (AGO)',
+        description:`The renowned Art Gallery of Ontario (AGO) is one of the largest museums in North America.
+            The collection of more than 95,000 pieces includes works from around the world, from European masterpieces to contemporary art, but also holds an impressive collection of Canadian Art, including a vast collection of works by the Group of Seven. A series of temporary exhibitions are mounted throughout the year.`,
+        image1:'https://images.unsplash.com/photo-1568151976576-fbac3729efd4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=924&q=80',
+        showMore:false
+    }
+]
